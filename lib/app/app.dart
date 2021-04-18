4c824a33694e68cff920913e1dc369f36ab1c4ac
ihni7 <ihnishat1998@gmail.com>
2021-04-18 07:44:42 +0600

    separated homepage

---
+++ lib/app/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../home/presentation/home_page.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
