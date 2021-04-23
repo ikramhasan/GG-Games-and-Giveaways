cb4810e4fce9816fe3dc67bd49522b038a37a8f4
ihni7 <ihnishat1998@gmail.com>
2021-04-23 20:17:48 +0600

    added thumbnail

--- lib/game_entity/presentation/game_enity_page.dart
+++ lib/game_entity/presentation/game_enity_page.dart
@@ -5,33 +5,34 @@ import 'package:free_games_giveaways/game_entity/data/cubit/game_entity_cubit.da
 class GameEntityPage extends StatelessWidget {
   final int id;
 
-  const GameEntityPage({Key? key,required this.id}) : super(key: key);
+  const GameEntityPage({Key? key, required this.id}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     final bloc = context.read<GameEntityCubit>();
     bloc.getGameEntity(id);
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: BlocConsumer<GameEntityCubit, GameEntityState>(
-          listener: (context, state) {
-            if (state is GameEntityError) {
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text(state.message)));
-            }
-          },
-          builder: (context, state) {
-            if (state is GameEntityLoading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (state is GameEntityLoaded) {
-              return Image.network(state.game.thumbnail);
-            }
-            return Container();
-          },
-        ),
+      body: BlocConsumer<GameEntityCubit, GameEntityState>(
+        listener: (context, state) {
+          if (state is GameEntityError) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text(state.message)));
+          }
+        },
+        builder: (context, state) {
+          if (state is GameEntityLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is GameEntityLoaded) {
+            return Image.network(
+              state.game.thumbnail,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            );
+          }
+          return Container();
+        },
       ),
     );
   }
